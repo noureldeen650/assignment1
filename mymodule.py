03d6be9cfc6b3a347476ea56aabbde78b3306950
noureldeen650 <internship2@ahli777.com>
2024-07-23 14:26:24 +0300

    This is a new commit for what I originally planned to be amended

---
+++ mymodule.py
@@ -0,0 +1,31 @@
+"""
+print("enter  the name")
+n=input()
+if n.isalpha():
+    print(f"your name is {n}")
+else:
+    print("invalid name")
+print("enter the age")
+a=input()
+if a.isdigit():
+    print(f"your age is {a}")
+else:
+    print("invalid age")
+print("enter the height")
+h=input()
+if h.isdigit():
+    print(f"your height is {h}")
+else:
+    print("invalid height")
+print("enter  the weight")
+w=input()
+if w.isdigit():
+    print(f"your weight is {w}")
+else:
+    print("invalid weight")
+
+"""
+
+import mymodule
+
+
