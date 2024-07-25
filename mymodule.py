34ece532a27dbbdc7418e20056297d85ff824897
Mosh Hamedani <internship2@ahli777.com>
2024-07-25 10:38:15 +0300

    Assignment1

--- mymodule.py
+++ mymodule.py
@@ -1,33 +1,85 @@
-"""
-print("enter  the name")
-n=input()
+Q1:
+
+print("Please enter your name")
+n = input()
+print("please enter your age")
+
+a = input()
+if len(n) < 5:
+    print("your name is " + n)
+else:
+    print("Name is too long")
+if a.isdigit():
+    print("your age is " + a)
+else:
+    print("invalid age")
+
+    Q2:
+print("Please enter the first number")
+n = input()
+print("please enter the second number")
+a = input()
+
+if n.isdigit() and a.isdigit():
+    x = int(a)
+    y = int(n)
+    print(y * x)
+    print(y + x)
+    print(y - x)
+    print(y / x)
+else:
+    print("please enter a number")
+
+Q3:
+
+print("enter the  value of first side of the rectangle")
+a = input()
+print("enter the value of the second  side ")
+b = input()
+
+if a.isdigit() and b.isdigit():
+    x = int(a)
+    y = int(b)
+    print(f"the area of the rectangle is {x * y} ")
+else:
+    print("invalid ")
+
+print("enter the circle Radius")
+r = input()
+if r.isdigit(rint(f"the area of the circle is  {2 * p * 3.14159}")
+
+Q4:
+
+    print("enter  the name")
+n = input()
 if n.isalpha():
     print(f"your name is {n}")
 else:
     print("invalid name")
 print("enter the age")
-a=input()
+a = input()
 if a.isdigit():
     print(f"your age is {a}")
 else:
     print("invalid age")
 print("enter the height")
-h=input()
+h = input()
 if h.isdigit():
     print(f"your height is {h}")
 else:
     print("invalid height")
 print("enter  the weight")
-w=input()
+w = input()
 if w.isdigit():
     print(f"your weight is {w}")
 else:
     print("invalid weight")
 
-"""
-
-import mymodule
-
-print("Nouraldeen")
-
-
+Q5:
+city = "Manchester"
+team = "United"
+club = city + team
+fullname = club
+modified_name = fullname.replace("United", "united")
+final_name = modified_name[:-2]):
+p = float(r)
